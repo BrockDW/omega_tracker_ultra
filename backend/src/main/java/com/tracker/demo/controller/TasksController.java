0dd20c0aa3b7fa4720f4cd0f9e45c0a8896a3ec6
BrockDW <brockdw@outlook.com>
2024-12-28 15:30:59 -0800

    update url

--- backend/src/main/java/com/tracker/demo/controller/TasksController.java
+++ backend/src/main/java/com/tracker/demo/controller/TasksController.java
@@ -2,6 +2,7 @@ package com.tracker.demo.controller;
 
 import com.tracker.demo.obj.Task;
 import com.tracker.demo.service.DailyTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @RestController
 public class TasksController {
 
+    @Autowired
     private final DailyTaskService dailyTaskService;
 
     public TasksController(DailyTaskService dailyTaskService) {
