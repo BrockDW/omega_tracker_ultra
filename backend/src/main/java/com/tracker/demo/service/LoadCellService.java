bdc502f653c31de000da7696c8f2f5e19e135bf9
BrockDW <brockdw@outlook.com>
2025-02-08 22:32:55 -0800

    adjust weight threashold

--- backend/src/main/java/com/tracker/demo/service/LoadCellService.java
+++ backend/src/main/java/com/tracker/demo/service/LoadCellService.java
@@ -20,7 +20,7 @@ public class LoadCellService {
     private boolean isExerciseActive = false;
 
     // Threshold to detect exercise (in kg)
-    private static final float EXERCISE_THRESHOLD = 2f;
+    private static final float EXERCISE_THRESHOLD = 1.5f;
 
     public void processWeightData(double weight) {
         if (weight >= EXERCISE_THRESHOLD && !isExerciseActive) {
