116d220f639915d8599c5580bfb5fe83a639bf1e
BrockDW <brockdw@outlook.com>
2025-02-08 18:48:06 -0800

    resolve cors error

--- backend/src/main/java/com/tracker/demo/config/WebConfig.java
+++ backend/src/main/java/com/tracker/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://192.168.0.10:3000/")
+                .allowedOrigins("http://localhost:3000", "http://192.168.0.10:3000/", "http://67.180.216.166:8080")
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*");
     }
